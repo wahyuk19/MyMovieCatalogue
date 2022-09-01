fbccda38b426f5066b9ecd933e5faf7e94fa5acd
wahyuk19 <wahyukrishna.tkj1@gmail.com>
2022-09-02 00:14:07 +0700

    add security

--- core/src/main/java/com/dicoding/mymoviecatalogue/core/di/DatabaseModule.kt
+++ core/src/main/java/com/dicoding/mymoviecatalogue/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): MovieTvDatabase = Room.databaseBuilder(
-        context,
-        MovieTvDatabase::class.java,"MovieTv.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): MovieTvDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MovieTvDatabase::class.java,"MovieTv.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideMovieTvDao(database: MovieTvDatabase):MovieTvDao = database.movieTvDao()
--- core/src/main/java/com/dicoding/mymoviecatalogue/core/di/NetworkModule.kt
+++ core/src/main/java/com/dicoding/mymoviecatalogue/core/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -20,10 +21,15 @@ class NetworkModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient{
         val loggingInterceptor = if(BuildConfig.DEBUG) { HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY) }else { HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE) }
+        val hostname = "*.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname,"sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .connectTimeout(120,TimeUnit.SECONDS)
             .readTimeout(120,TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
